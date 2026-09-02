8b216ba43585640a7a55f6814f4193dee2755760
Andrea L. Pazo Sanchez <APazo@onpe.gob.pe>
2026-09-01 19:50:25 -0500

    Tabla de Multiplicar

---
+++ SEMANA-1/Andrea/Ejercicio_03.py
@@ -0,0 +1,7 @@
+# Tabla de Multiplicar
+# Solicita un número y muestra su tabla de multiplicar del 1 al 10 usando una estructura repetitiva Para.
+
+num = int(input("Ingrese un número: "))
+
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
