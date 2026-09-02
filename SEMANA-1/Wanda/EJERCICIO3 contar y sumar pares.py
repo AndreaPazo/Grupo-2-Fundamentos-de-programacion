aa2a3b56b28c99970e8c9ea702d52a520749e8be
Wand <119546115+wandarm@users.noreply.github.com>
2026-09-01 23:45:06 -0500

    Update EJERCICIO3 contar y sumar pares.py

--- SEMANA-1/Wanda/EJERCICIO3 contar y sumar pares.py
+++ SEMANA-1/Wanda/EJERCICIO3 contar y sumar pares.py
@@ -1,23 +1,15 @@
-#================================================
-#EJERCICIO 04 - CONTAR Y SUMAR PARES DEL 1 AL 20
-#Estructura Repetitiva "Para" (for) + CONDICIONAL (if)
+#===============================================
+#EJERCICIO 03 - TABLA DE MULTIPLICAR
+#Estructura Repetitiva 
 #==================================================
-
-#inicia en 0 porque todavía no hemos contado ni sumado nada
-contador_pares = 0
-suma_pares = 0
-
-#Se usa un bucle for para que recorra del 1 al 20 
-#range(1, 21) -> del 1 al 20 ( 21 no se incluye)
-for numero in range(1, 21):
-
-    # Verificamos si el número es par
-    # Un número es par cuando el resto de dividirlo entre 2 es 0
-    if numero % 2 == 0:
-        contador_pares = contador_pares + 1   # sumamos 1 al contador
-        suma_pares = suma_pares + numero      # acumulamos el número a la suma
-
-#Resultado
-print("*****************************")
-print(f"Cantidad de números pares encontrados: {contador_pares}")
-print(f"Suma total de los números pares: {suma_pares}")
+#Datos
+numero = int(input("Ingrese un número para ver su tabla de multiplicar: "))
+ 
+#range(1, 11) genera los números del 1 al 10
+#(el 11 no se incluye, por eso ponemos uno más del límite que queremos)
+print(f"--- Tabla del {numero} ---")
+ 
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
+ 
